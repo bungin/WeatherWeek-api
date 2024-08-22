24e7f85a16cf01607aa33ed09e13414eb17b687e
bungin <nimai.band@gmail.com>
2024-08-22 04:51:58 -0500

    fill fetchlocationdata, weatherservice

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,28 +1,58 @@
 import dotenv from 'dotenv';
+import { response } from 'express';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
 interface Coordinates {
   lat: number;
-  lon: number; 
+  lon: number;
 }
 // TODO: Define a class for the Weather object
 class Weather {
+  private temp: number;
+  private wind: number;
+  private humidity: number;
 
+  constructor(temp: number, wind: number, humidity: number) {
+    this.temp = temp;
+    this.wind = wind;
+    this.humidity = humidity;
+  }
 }
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
-  private baseURL?: string;
-  private APIkey?: string;
-  private cityName?: string;
+  private baseURL: string;
+  private APIkey: string; //appid
+  private cityName: string;
+  private lat: number;
+  private lon: number; // needed? defined in coordinates as well.
 
-  constructor() {
-    //?
+  constructor(baseURL: string, APIkey: string, cityName: string, lat: number, lon: number) {
+    this.baseURL = baseURL;
+    this.APIkey = APIkey;
+    this.cityName = cityName;
+    this.lat = lat;
+    this.lon = lon;
   }
   // TODO: Create fetchLocationData method
 
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    const APIkey = "6dfef76b1f1270428b5bdfb531c0f100"
+    const url = `api.openweathermap.org/data/2.5/forecast?lat=${this.lat}&lon=${this.lon}&appid=${this.APIkey}`
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch location data');
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching location data:', error);
+      return null;
+    }
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
